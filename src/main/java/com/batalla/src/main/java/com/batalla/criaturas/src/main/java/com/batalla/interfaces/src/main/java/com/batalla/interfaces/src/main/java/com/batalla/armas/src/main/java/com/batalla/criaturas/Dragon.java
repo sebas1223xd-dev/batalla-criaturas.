1cd5a31c6ad73bdc81e16672cebb244df8222bfb
sebas1223xd-dev <sebas1223xd@gmail.com>
2026-04-20 22:28:47 -0500

    Update Dragon.java

--- src/main/java/com/batalla/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/armas/src/main/java/com/batalla/criaturas/Dragon.java
+++ src/main/java/com/batalla/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/armas/src/main/java/com/batalla/criaturas/Dragon.java
@@ -25,3 +25,29 @@ public class Dragon extends Criatura implements Volador {
         System.out.println(nombre + " aterriza");
     }
 }
+public class Dragon extends Criatura implements Volador {
+
+    public Dragon(String nombre, int salud, int fuerza) {
+        super(nombre, salud, fuerza);
+    }
+
+    public void atacar(Criatura objetivo) {
+        int daño = fuerza * 2;
+        if (arma != null) daño += arma.getDañoAdicional();
+        System.out.println(nombre + " ataca 🔥");
+        objetivo.defender(daño);
+    }
+
+    public void defender(int daño) {
+        salud -= daño;
+        System.out.println(nombre + " vida: " + salud);
+    }
+
+    public void volar() {
+        System.out.println(nombre + " vuela");
+    }
+
+    public void aterrizar() {
+        System.out.println(nombre + " aterriza");
+    }
+}
