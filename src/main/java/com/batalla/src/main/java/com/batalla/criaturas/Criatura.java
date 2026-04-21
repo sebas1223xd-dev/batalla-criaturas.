70364b11140d7730258c434a7b1a1475ec70a9c2
sebas1223xd-dev <sebas1223xd@gmail.com>
2026-04-20 22:26:06 -0500

    Update Criatura.java

--- src/main/java/com/batalla/src/main/java/com/batalla/criaturas/Criatura.java
+++ src/main/java/com/batalla/src/main/java/com/batalla/criaturas/Criatura.java
@@ -29,3 +29,26 @@ public abstract class Criatura {
         this.arma = arma;
     }
 }
+public abstract class Criatura {
+    protected String nombre;
+    protected int salud;
+    protected int fuerza;
+    protected Arma arma;
+
+    public Criatura(String nombre, int salud, int fuerza) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.fuerza = fuerza;
+    }
+
+    public abstract void atacar(Criatura objetivo);
+    public abstract void defender(int daño);
+
+    public boolean estaViva() {
+        return salud > 0;
+    }
+
+    public void equiparArma(Arma arma) {
+        this.arma = arma;
+    }
+}
