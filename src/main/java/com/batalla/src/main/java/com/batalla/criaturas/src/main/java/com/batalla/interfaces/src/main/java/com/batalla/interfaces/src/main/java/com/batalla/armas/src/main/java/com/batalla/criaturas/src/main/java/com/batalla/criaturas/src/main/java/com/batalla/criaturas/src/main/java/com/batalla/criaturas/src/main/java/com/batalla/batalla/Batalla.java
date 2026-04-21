b419af5b2f3d06daef40681616c12f92df8a3600
sebas1223xd-dev <sebas1223xd@gmail.com>
2026-04-20 22:30:40 -0500

    Update Batalla.java

--- src/main/java/com/batalla/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/armas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/batalla/Batalla.java
+++ src/main/java/com/batalla/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/armas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/batalla/Batalla.java
@@ -21,3 +21,22 @@ public class Batalla {
         }
     }
 }
+public class Batalla {
+
+    public static void pelear(Criatura c1, Criatura c2) {
+
+        while (c1.estaViva() && c2.estaViva()) {
+            c1.atacar(c2);
+
+            if (c2.estaViva()) {
+                c2.atacar(c1);
+            }
+        }
+
+        if (c1.estaViva()) {
+            System.out.println("Gana " + c1.nombre);
+        } else {
+            System.out.println("Gana " + c2.nombre);
+        }
+    }
+}
