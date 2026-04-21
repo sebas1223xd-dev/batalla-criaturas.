534deb7ffabce28ec6a33d78f3cd1c603f06a53c
sebas1223xd-dev <sebas1223xd@gmail.com>
2026-04-20 21:07:49 -0500

    Create Mago.java
    Clase Mago

---
+++ src/main/java/com/batalla/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/armas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/Mago.java
@@ -0,0 +1,27 @@
+package com.batalla.criaturas;
+
+import com.batalla.interfaces.Magico;
+
+public class Mago extends Criatura implements Magico {
+
+    public Mago(String nombre, int salud, int fuerza) {
+        super(nombre, salud, fuerza);
+    }
+
+    @Override
+    public void atacar(Criatura objetivo) {
+        int daño = fuerza;
+        if (arma != null) daño += arma.getDaño();
+
+        System.out.println(nombre + " usa magia ✨");
+        objetivo.defender(daño);
+    }
+
+    public void lanzarHechizo() {
+        System.out.println(nombre + " lanza hechizo");
+    }
+
+    public void aprenderHechizo() {
+        System.out.println(nombre + " aprende hechizo");
+    }
+}
