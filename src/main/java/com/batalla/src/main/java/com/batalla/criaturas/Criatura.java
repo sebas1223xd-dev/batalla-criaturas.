f6a956918a74aaf53df13b3c3609d379ac5a0a54
sebas1223xd-dev <sebas1223xd@gmail.com>
2026-04-20 21:01:27 -0500

    Create Criatura.java
    Clase abstracta Criatura

---
+++ src/main/java/com/batalla/src/main/java/com/batalla/criaturas/Criatura.java
@@ -0,0 +1,31 @@
+package com.batalla.criaturas;
+
+import com.batalla.armas.Arma;
+
+public abstract class Criatura {
+    protected String nombre;
+    protected int salud;
+    protected int fuerza;
+    protected Arma arma;
+
+    public Criatura(String nombre, int salud, int fuerza) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.fuerza = fuerza;
+    }
+
+    public abstract void atacar(Criatura objetivo);
+
+    public void defender(int daño) {
+        salud -= daño;
+        System.out.println(nombre + " recibe " + daño + " daño. Vida: " + salud);
+    }
+
+    public boolean estaViva() {
+        return salud > 0;
+    }
+
+    public void equiparArma(Arma arma) {
+        this.arma = arma;
+    }
+}
