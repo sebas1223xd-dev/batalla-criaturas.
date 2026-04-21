37da491d01c328b9711b96eb8af67f5288b7f949
sebas1223xd-dev <sebas1223xd@gmail.com>
2026-04-20 22:29:46 -0500

    Update Guerrero.java

--- src/main/java/com/batalla/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/armas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/Guerrero.java
+++ src/main/java/com/batalla/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/armas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/Guerrero.java
@@ -15,3 +15,21 @@ public class Guerrero extends Criatura {
         objetivo.defender(daño);
     }
 }
+public class Guerrero extends Criatura {
+
+    public Guerrero(String nombre, int salud, int fuerza) {
+        super(nombre, salud, fuerza);
+    }
+
+    public void atacar(Criatura objetivo) {
+        int daño = fuerza;
+        if (arma != null) daño += arma.getDañoAdicional();
+        System.out.println(nombre + " ataca ⚔️");
+        objetivo.defender(daño);
+    }
+
+    public void defender(int daño) {
+        salud -= daño;
+        System.out.println(nombre + " vida: " + salud);
+    }
+}
