ed664f67a1ff87fb4109f09c98980d9d62e93327
sebas1223xd-dev <sebas1223xd@gmail.com>
2026-04-20 22:29:24 -0500

    Update Mago.java

--- src/main/java/com/batalla/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/armas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/Mago.java
+++ src/main/java/com/batalla/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/interfaces/src/main/java/com/batalla/armas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/src/main/java/com/batalla/criaturas/Mago.java
@@ -25,3 +25,27 @@ public class Mago extends Criatura implements Magico {
         System.out.println(nombre + " aprende hechizo");
     }
 }
+public class Mago extends Criatura implements Magico {
+
+    public Mago(String nombre, int salud, int fuerza) {
+        super(nombre, salud, fuerza);
+    }
+
+    public void atacar(Criatura objetivo) {
+        int daño = fuerza;
+        if (arma != null) daño += arma.getDañoAdicional();
+        lanzarHechizo();
+        objetivo.defender(daño);
+    }
+
+    public void defender(int daño) {
+        salud -= daño;
+        System.out.println(nombre + " vida: " + salud);
+    }
+
+    public void lanzarHechizo() {
+        System.out.println(nombre + " lanza hechizo ✨");
+    }
+
+    public void aprenderHechizo(String h) {}
+}
